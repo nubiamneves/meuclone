cc082d1c572e638f0c524f0ff5c3a151de2010e4
nubiamneves <nubianeves950@gmail.com>
2022-11-16 21:49:02 -0300

    consertando pag2

---
+++ src/screen/Pag2/styles.ts
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'white',
+        paddingTop: 64,
+        paddingHorizontal: 10
+    },
+    title: {
+        color: 'black',
+        fontSize: 20,
+        fontWeight: 'bold',
+        textAlign: "center"
+    },
+})
