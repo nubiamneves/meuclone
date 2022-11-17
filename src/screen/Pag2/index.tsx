6854535245359551d9bca7202a4491586265d96e
nubiamneves <nubianeves950@gmail.com>
2022-11-16 21:38:20 -0300

    atualizando index pag2

--- src/screen/Pag2/index.tsx
+++ src/screen/Pag2/index.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { View, Text, TouchableOpacity, Image, TextInput } from 'react-native';
+import { styles } from './styles'; 
 import { Ionicons } from '@expo/vector-icons';
 import lua from '../../assets/lua.webp'
 
 export const Pag2= () => {
 
     return (
-      <View>
+      <View style={styles.container}>
         <View>
         <Ionicons name="arrow-back" size={24} color="black" />
-        <Text>
+        <Text style={styles.title}>
             Meu artigo de teste
         </Text>
        </View>
